2d89a57c3a267208be0f6647d73d9061a6399523
qsrdev <matteo.albertini747@gmail.com>
2025-06-09 16:16:59 +0200

    feat: task end

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import moovies from "../data/array.js";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [selectedGenre, setselectedGenre] = useState("No Filter");
+  const genres = ["No Filter", "Fantascienza", "Thriller", "Romantico", "Azione"];
+  const [mooviesFiltered, setmooviesFiltered] = useState(moovies);
+
+  useEffect(() => {
+    if (selectedGenre === "No Filter") {
+      setmooviesFiltered(moovies);
+    } else {
+      const newMooviesFilter = moovies.filter((curMoovie) => selectedGenre === curMoovie.genre);
+      setmooviesFiltered(newMooviesFilter);
+    }
+  }, [selectedGenre]);
 
   return (
     <>
       <div>
         <h1 className="text-center">Moovies</h1>
         <div className="container my-3">
+          <select value={selectedGenre} onChange={(event) => setselectedGenre(event.target.value)}>
+            {genres.map((curGenre, index) => (
+              <option key={index} value={curGenre}>
+                {curGenre}
+              </option>
+            ))}
+          </select>
           <ul className="list-group">
-            {moovies.map((curMoovie, index) => (
+            {mooviesFiltered.map((curMoovie, index) => (
               <li key={index} className="list-group-item">
                 {curMoovie.title} - {curMoovie.genre}
               </li>
